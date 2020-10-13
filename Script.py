5e9bc7534198ca9f2f41b6aff1890eb16e0c4ac6
Aaron <archiechocolate@gmail.com>
2020-10-13 21:08:49 +0100

    added comments to Script.py

--- Script.py
+++ Script.py
@@ -1,15 +1,26 @@
+#import the numpy module
 import numpy as numpy
 
+#Ask user for degree of polynomial
 degree = int(input("Degree:"))
+
+#create a list called 'coeffs'
 coeffs = []
 
+#iterate the degree + 1 times, asking for the coefficient of each degree.
 for c in range (0,degree+1):
         coefficient = int(input("number: "))
+        
+        #append the inputs to the 'coeffs' list created on line 8
         coeffs.append(coefficient)
-        #print(coeffs)        
-
+               
+#Put coefficients in the form of a polynomial
 p = numpy.poly1d(coeffs)
 print(p)
+
+#Use numpy to calculate roots
 roots = p.roots
+
+#Output the result
 print("Roots:", roots)
 Exit = input("PRESS ANY BUTTON TO EXIT")
